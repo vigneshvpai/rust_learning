4d3b35b3fc40312a805227c4f0fae7e9b281f577
Pai, Vignesh Vivek <vignesh.vivek.pai@iis.fraunhofer.de>
2026-02-12 12:20:32 +0100

    Type conversion for guess to u32 which was a string

--- src/chapter_2.rs
+++ src/chapter_2.rs
@@ -18,11 +18,13 @@ pub fn app() {
         .read_line(&mut guess)
         .expect("Failed to read line!");
 
+    let guess: u32 = guess.trim().parse().expect("Pleaes type a number");
+
+    println!("You guessed: {guess}");
+
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Equal => println!("You win!"),
         Ordering::Greater => println!("Too big!"),
     }
-
-    println!("You guessed: {guess}");
 }
