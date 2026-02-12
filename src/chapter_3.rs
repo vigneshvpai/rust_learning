1b9be33cf8f0469fe30db17ebe4ed21e4ff2cac7
Vignesh V Pai <vigneshpai444@gmail.com>
2026-02-12 14:03:51 +0100

    Constatnts

--- src/chapter_3.rs
+++ src/chapter_3.rs
@@ -1,6 +1,9 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
 pub fn app() {
     let mut x = 5;
     println!("This is the value of {x}");
     x = 6;
     println!("This is the value of {x}");
+    println!("{THREE_HOURS_IN_SECONDS}");
 }
