2a38b62a8ba4192f27a9c47214bf2dbca852b78b
Vignesh V Pai <vigneshpai444@gmail.com>
2026-02-12 15:17:23 +0100

    Completed Data Types section 3.2

--- src/chapter_3.rs
+++ src/chapter_3.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 pub fn three_one() {
@@ -85,4 +87,46 @@ pub fn three_two() {
     let unit_value = ();
 
     let a = [1, 2, 3, 4, 5];
+
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let a = [3; 5]; // [3,3,3,3,3]
+}
+
+pub fn index_out_of_array() {
+    // This code will panic in Rust, but in C++ it will give some ghost value.
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod chapter_3;
 fn main() {
     // chapter_2::app();
     // chapter_3::three_one();
-    chapter_3::three_two();
+    // chapter_3::three_two();
+    chapter_3::index_out_of_array();
 }
