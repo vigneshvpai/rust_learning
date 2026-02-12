7a8c206d0358b267f9987347d117b0e8ce16216c
Vignesh V Pai <vigneshvpai@gmail.com>
2026-02-12 12:33:42 +0100

    Handled non number inputs using match and Result enum

--- src/chapter_2.rs
+++ src/chapter_2.rs
@@ -19,7 +19,10 @@ pub fn app() {
             .read_line(&mut guess)
             .expect("Failed to read line!");
 
-        let guess: u32 = guess.trim().parse().expect("Pleaes type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         // println!("You guessed: {guess}");
 
