d84afd68b1f80a4d661bfcb4d42f1451fdd6fc4c
Vignesh V Pai <vigneshpai444@gmail.com>
2026-03-03 15:04:18 +0100

    Learnt error handling using Result

--- src/chapter_9.rs
+++ src/chapter_9.rs
@@ -1,13 +1,57 @@
 use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self, Read};
 
 pub fn sub_1() {
     // let v = vec![1, 2, 3];
 
     // v[99];
 
-    let greeting_file_result = File::open("hello.txt");
-    match greeting_file_result {
-        Ok(file) => println!("{:?}", file),
-        Err(err) => println!("{:?}", err),
+    // let greeting_file_result = File::open("hello.txt");
+    // match greeting_file_result {
+    //     Ok(file) => println!("{:?}", file),
+    //     Err(err) => println!("{:?}", err),
+    // }
+
+    // let greeting_file_result = File::open("hello.txt");
+
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {e:?}"),
+    //         },
+    //         _ => {
+    //             panic!("Problem opening the file: {error:?}");
+    //         }
+    //     },
+    // };
+
+    // let greeting_file = File::open("hello.txt").unwrap();
+    let greeting_file =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
     }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
